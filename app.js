ac3d3b8c242de138d17e3b695788ba09da738097
Kashyap <kash1469@gmail.com>
2020-04-15 17:24:14 +0530

    Functioning

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
 (function () {
 'use strict';
 
-angular.module('myFirstApp', [])
+angular.module('LunchCheck', [])
 
-.controller('MyFirstController', function ($scope) {
-$scope.name="Kashyap";
-$scope.sayHello = function () {
-	return "Hello Kashyap!";
-};
-});
+.controller('LunchCheckController', LunchCheckController);
+
+LunchCheckController.$inject = ['$scope'];
+function LunchCheckController($scope) {
+    $scope.LunchCheck = "";
+    $scope.message = "";
+    $scope.Check = function () {
+        var Lunch = $scope.LunchCheck;
+        if (Lunch === "") {
+            $scope.msgstyle = { "color": "Red" };
+            $scope.message = "Please enter data first";
+        }
+        else {
+            var LC = Lunch.replace(/,\s*$/, "").split(',');
+            if (LC.length <= 3) {
+                $scope.msgstyle = { "color": "Green" };
+                $scope.message = LC.length + " Items. Enjoy!";
+            }
+            else {
+                $scope.msgstyle = { "color": "Green" };
+                $scope.message = "Too Much";
+            }
+        }
+
+    };
+}
 
 })();
