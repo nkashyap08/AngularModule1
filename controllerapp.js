410ca97c70e092c0a8a71111338dd3c9cc8c289b
Kashyap <kash1469@gmail.com>
2020-04-18 18:16:31 +0530

    SyntaxController

---
+++ controllerapp.js
@@ -0,0 +1,18 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('SyntaxController', [])
+    .controller('ParentController', ParentController)
+    .controller('ChildController', ChildController);
+
+    function ParentController() {
+        var parent = this;
+        parent.value = 1;
+    }
+
+    function ChildController() {
+        var child = this;
+        child.value = 5;
+    }
+
+})();
